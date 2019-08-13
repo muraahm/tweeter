8c428a0e73933c042752bd53cd135cba61033e75
muraahm <ahmedmuraahm121@gmail.com>
2019-08-13 21:37:47 +0000

    fix counter updates and turns red after 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-  $("#text").on('keydown', function() {
+  $('.new-tweet form').on('keydown', 'textarea', function(event) {
     const max = 140;
     let length = $(this).val().length; 
-    if (length <= maxLength) {
+    if (length <= max) {
       $(this).parent().children('.counter').html(max - length).removeClass("red");
     } else {
       $(this).parent().children('.counter').html(max - length).addClass("red");
