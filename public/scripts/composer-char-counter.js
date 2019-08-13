673657a69e896ad231b2454bd935a3136557d08e
muraahm <ahmedmuraahm121@gmail.com>
2019-08-13 21:44:08 +0000

    refactor counter updates function and turns red after 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $('.new-tweet form').on('keydown', 'textarea', function(event) {  
-    const max = 140;
-    let length = $(this).val().length; 
-    if (length <= max) {
-      $(this).parent().children('.counter').html(max - length).removeClass("red");
-    } else {
-      $(this).parent().children('.counter').html(max - length).addClass("red");
-    }
-  });
-
-});
+  $('.new-tweet form').on('keydown', 'textarea', function(event) {
+  const count = $(this).parent().find('.counter');
+  const counter = 140 - $(this).val().length;
+  count.html(counter);
+    if(counter >= 0) {
+      $(count).css('color', 'black');
+       } else {
+         $(count).css('color', 'red');
+      }
+  })
+ });
