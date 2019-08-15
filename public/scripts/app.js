14835d1f8d34094a3d970633e2ab4744b9eb7661
muraahm <ahmedmuraahm121@gmail.com>
2019-08-15 20:31:13 +0000

    displaying validation errors

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,56 +29,56 @@
 // ];
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-const timeDifference = function(current, previous) {  
-  const msPerMinute = 60 * 1000;
-  const msPerHour = msPerMinute * 60;
-  const msPerDay = msPerHour * 24;
-  const msPerMonth = msPerDay * 30;
-  const msPerYear = msPerDay * 365;
+  const timeDifference = function (current, previous) {
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
 
-  const elapsed = current - previous;
-  let plural = 's';
+    const elapsed = current - previous;
+    let plural = 's';
 
-  if (elapsed < msPerMinute) {
-    const seconds = Math.round(elapsed / 1000);
-    if (seconds === 1) plural = '';
-    return seconds + ' second' + plural + ' ago';
-  } else if (elapsed < msPerHour) {
-    const minutes = Math.round(elapsed / msPerMinute);
-    if (minutes === 1) plural = '';
-    return minutes + ' minute' + plural + ' ago';
-  } else if (elapsed < msPerDay) {
-    const hours = Math.round(elapsed / msPerHour);
-    if (hours === 1) plural = '';
-    return hours + ' hour' + plural + ' ago';
-  } else if (elapsed < msPerMonth) {
-    const days = Math.round(elapsed / msPerDay);
-    if (days === 1) plural = '';
-    return days + ' day' + plural + ' ago';
-  } else if (elapsed < msPerYear) {
-    const months = Math.round(elapsed / msPerMonth);
-    if (months === 1) plural = '';
-    return months + ' month' + plural + ' ago';
-  } else {
-    const years = Math.round(elapsed / msPerYear);
-    if (years === 1) plural = '';
-    return years + ' year' + plural + ' ago';
+    if (elapsed < msPerMinute) {
+      const seconds = Math.round(elapsed / 1000);
+      if (seconds === 1) plural = '';
+      return seconds + ' second' + plural + ' ago';
+    } else if (elapsed < msPerHour) {
+      const minutes = Math.round(elapsed / msPerMinute);
+      if (minutes === 1) plural = '';
+      return minutes + ' minute' + plural + ' ago';
+    } else if (elapsed < msPerDay) {
+      const hours = Math.round(elapsed / msPerHour);
+      if (hours === 1) plural = '';
+      return hours + ' hour' + plural + ' ago';
+    } else if (elapsed < msPerMonth) {
+      const days = Math.round(elapsed / msPerDay);
+      if (days === 1) plural = '';
+      return days + ' day' + plural + ' ago';
+    } else if (elapsed < msPerYear) {
+      const months = Math.round(elapsed / msPerMonth);
+      if (months === 1) plural = '';
+      return months + ' month' + plural + ' ago';
+    } else {
+      const years = Math.round(elapsed / msPerYear);
+      if (years === 1) plural = '';
+      return years + ' year' + plural + ' ago';
+    }
+  };
+
+  const escape = function (str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
-};
 
-const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-const createTweetElement = function (obj) {
-  const currentDate = new Date();
-  const text = escape(obj.content.text);
-  const $markup =
-    $(`<article class="article">
+  const createTweetElement = function (obj) {
+    const currentDate = new Date();
+    const text = escape(obj.content.text);
+    const $markup =
+      $(`<article class="article">
   <img src="${obj.user.avatars}">
   <h3> ${obj.user.name} </h3>
   <h3 class="tag"> ${obj.user.handle} </h3>
@@ -87,63 +87,74 @@ const createTweetElement = function (obj) {
       <div class="btm-icon"> <a href="#"><i class="fas fa-flag"></i></a> <a href="#"><i class="fas fa-retweet"></i></a> <a href="#"><i class="fas fa-heart"></i></a> </div>
     </footer>
 </article>`);
-  return $markup;
-};
+    return $markup;
+  };
 
 
-const renderTweets = function (tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (const tweet of tweets) {
-    $(".tweet").append(createTweetElement(tweet));
-  }
-};
-
-const loadTweets = function() {
-  return $.ajax({
-    url: '/tweets/',
-    type: "GET",
-    success: function(data) {
-      //console.log(data);
-      renderTweets(data);
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      $(".tweet").append(createTweetElement(tweet));
     }
-  });
-};
+  };
 
-$(function() {
-  $('#form').on("submit", function(event) {
-    event.preventDefault();
-    const $form = $(this);
-  const data = $form.serialize();
-    //console.log('Button clicked, performing ajax call...');
-    const tweetMsg = data.substring(5)
-    if (!tweetMsg) {
-      alert("Tweet box can't be empty!!")
-    }
-    if (tweetMsg.length > 140) {
-      alert("You've reached the max char")
-    }
-    else
-
-    $.ajax({
-      type: 'POST',
-      url: "/tweets/",
-      data: data
-    })
-    .then(function() {
-      loadTweets()
-      $('#tweetMsg').val('');
-      //console.log('Success: ', data);
-      
-    })
-    .fail(function(err) {
-      console.log(err)
+  const loadTweets = function () {
+    return $.ajax({
+      url: '/tweets/',
+      type: "GET",
+      success: function (data) {
+        //console.log(data);
+        renderTweets(data);
+      }
     });
+  };
+
+
+  //setTimeout(function(){ $(".error").slideUp(); }, 1000);
+
+  $(function () {
+    $('#form').on("submit", function (event) {
+      event.preventDefault();
+      const $form = $(this);
+      const data = $form.serialize();
+      //console.log('Button clicked, performing ajax call...');
+      const tweetMsg = data.substring(5)
+      if (!tweetMsg) {
+        $(".err").slideDown();
+        $(".err span").text("Tweet box can't be empty.");
+      } else if (tweetMsg.length > 140) {
+        $(".err").slideDown();
+        $(".err span").text("You've reached the max char.");
+      } else {
+        $(".err").slideUp();
+        $.ajax({
+            type: 'POST',
+            url: "/tweets/",
+            data: data
+          })
+          .then(function () {
+            loadTweets()
+            $('#tweetMsg').val('');
+            //console.log('Success: ', data);
+
+          })
+          .fail(function (err) {
+            console.log(err)
+          });
+      }
+    });
+
   });
 });
-});
 
+$("#arrow-nav").click(function (event) {
+  event.preventDefault();
+  $(".new-tweet").toggle(200, "swing");
+  $("textarea#text").focus();
+  return false;
+});
 
 
 
