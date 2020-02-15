051984e11e470bf96ed69b91518c4190b603b108
Ahmed <sultan_902002@yahoo.com>
2020-02-15 16:05:31 -0700

    change port config for heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 "use strict";
 
 // Basic express setup:
-
-const PORT          = 8080;
+const PORT = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
